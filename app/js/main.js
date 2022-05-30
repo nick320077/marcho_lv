f148713e96ab39d3810d8ec1fe4d155889174a38
nick32007 <kobets32@gmail.com>
2022-05-30 18:24:52 +0300

    added product section

--- app/js/main.js
+++ app/js/main.js
@@ -5,4 +5,11 @@ $(function () {
         autoplay: true,
         fade: true,
     });
+
+    $(".star").rateYo({
+        starWidth: "17px",
+        normalFill: "#ccccce",
+        ratedFill: "#ffc35b",
+        readOnly: true
+    });
 });
