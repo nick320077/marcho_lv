e40f32a763515ab86c019dbbe44ec81c024bac13
nick32007 <kobets32@gmail.com>
2022-06-05 21:43:39 +0300

    added product page

--- app/js/main.js
+++ app/js/main.js
@@ -23,9 +23,7 @@ $(function () {
         $(".product-tabs__top-item").removeClass("product-tabs__top-item--active");
         $(this).addClass("product-tabs__top-item--active");
 
-        $(".product-tabs__content-item").removeClass(
-            "product-tabs__content-item--active"
-        );
+        $(".product-tabs__content-item").removeClass("product-tabs__content-item--active");
         $($(this).attr("href")).addClass("product-tabs__content-item--active");
     });
 
